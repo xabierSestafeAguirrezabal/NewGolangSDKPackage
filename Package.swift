6c724eed99c58967ce8b2a89968ffe080804becf
Xabier Sestafe <xabier.sestafe@ironchip.com>
2023-04-19 12:18:51 +0200

    WIP

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NewGolangSDKPackage",
-            targets: ["NewGolangSDKPackage"]),
+            targets: ["ironchipauthenticatorgolangsdk"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +18,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "NewGolangSDKPackage",
-            dependencies: []),
-        .testTarget(
-            name: "NewGolangSDKPackageTests",
-            dependencies: ["NewGolangSDKPackage"]),
+        .binaryTarget(
+                  name: "ironchipauthenticatorgolangsdk",
+                  path: "Frameworks/ironchipauthenticatorgolangsdk.xcframework"
+              ),
     ]
 )
